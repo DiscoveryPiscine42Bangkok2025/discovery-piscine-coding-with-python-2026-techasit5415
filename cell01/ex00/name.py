111990245e707979debf3d8d0871b400e323a0c0
root <root@Techasit.localdomain>
2026-02-11 02:41:30 +0700

    ex 00 01 02 03

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Techasit"
+last_name = "vanitpattarakul"
+print(first_name,last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Techasit"
+last_name = "vanitpattarakul"
+whole_name = first_name + " " + last_name
+print(whole_name)
